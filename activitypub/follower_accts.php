4cc7344383ff70c3bd4d4769dbaac59a0d6e0b44
Julian Lemmerich <41118534+JM-Lemmi@users.noreply.github.com>
2024-05-10 17:53:57 +0200

    count followers the same way the list is generated

--- activitypub/follower_accts.php
+++ activitypub/follower_accts.php
@@ -2,7 +2,7 @@
   "@context": "https://www.w3.org/ns/activitystreams",
   "id": "https://blog.julian-lemmerich.de/activitypub/follower_accts.php",
   "type": "OrderedCollectionPage",
-  "totalItems": <?php echo count(file('followers.txt')); ?>,
+  "totalItems": <?php echo count(file('followers.txt', FILE_IGNORE_NEW_LINES + FILE_SKIP_EMPTY_LINES)); ?>,
   "partOf": "https://blog.julian-lemmerich.de/activitypub/follower_accts.php",
   "orderedItems": <?php echo json_encode(file('followers.txt', FILE_IGNORE_NEW_LINES + FILE_SKIP_EMPTY_LINES)); ?>
 }
--- activitypub/followers.php
+++ activitypub/followers.php
@@ -2,6 +2,6 @@
     "@context": "https://www.w3.org/ns/activitystreams",
     "id": "https://blog.julian-lemmerich.de/activitypub/followers.php",
     "type": "OrderedCollection",
-    "totalItems": <?php echo count(file('followers.txt')); ?>,
+    "totalItems": <?php echo count(file('followers.txt', FILE_IGNORE_NEW_LINES + FILE_SKIP_EMPTY_LINES)); ?>,
     "first": "https://blog.julian-lemmerich.de/activitypub/follower_accts.php"
 }
